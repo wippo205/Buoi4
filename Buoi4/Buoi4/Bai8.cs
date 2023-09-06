7c2accda6b3caaef182ce5a721252975b238fa85
NguyenHuuDai <h1h1macon@gmail.com>
2023-09-06 16:24:36 +0700

    update:
    fa

---
+++ Buoi4/Buoi4/Bai8.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi4
+{
+class CD
+{
+    private int maCD;
+    private string tuaCD;
+    private int soBaiHat;
+    private double giaThanh;
+
+    public CD()
+    {
+        maCD = 999999;
+        tuaCD = "chưa xác định";
+        soBaiHat = 0;
+        giaThanh = 0.0;
+    }
+
+    public CD(int maCD, string tuaCD, int soBaiHat, double giaThanh)
+    {
+        if (maCD > 0)
+            this.maCD = maCD;
+        else
+            this.maCD = 999999;
+
+        if (!string.IsNullOrEmpty(tuaCD))
+            this.tuaCD = tuaCD;
+        else
+            this.tuaCD = "chưa xác định";
+
+        if (soBaiHat > 0)
+            this.soBaiHat = soBaiHat;
+        else
+            this.soBaiHat = 0;
+
+        if (giaThanh > 0)
+            this.giaThanh = giaThanh;
+        else
+            this.giaThanh = 0.0;
+    }
+
+    public int MaCD
+    {
+        get { return maCD; }
+        set
+        {
+            if (value > 0)
+                maCD = value;
+        }
+    }
+
+    public string TuaCD
+    {
+        get { return tuaCD; }
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+                tuaCD = value;
+        }
+    }
+
+    public int SoBaiHat
+    {
+        get { return soBaiHat; }
+        set
+        {
+            if (value > 0)
+                soBaiHat = value;
+        }
+    }
+
+    public double GiaThanh
+    {
+        get { return giaThanh; }
+        set
+        {
+            if (value > 0)
+                giaThanh = value;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "Mã CD: "+maCD + "Tựa CD: "+tuaCD+ "Số bài hát: "+soBaiHat+ "Giá thành: "+giaThanh;
+    }
+}
+
+class CDList
+{
+    private CD[] cds;
+    private int maxSize;
+    private int currentSize;
+
+    public CDList(int n)
+    {
+        maxSize = n;
+        cds = new CD[maxSize];
+        currentSize = 0;
+    }
+
+    public bool AddCD(CD cd)
+    {
+        if (currentSize >= maxSize || cds.Any(x => x != null && x.MaCD == cd.MaCD))
+            return false;
+
+        cds[currentSize] = cd;
+        currentSize++;
+        return true;
+    }
+
+    public int Count()
+    {
+        return currentSize;
+    }
+
+    public double TotalCost()
+    {
+        return cds.Where(x => x != null).Sum(x => x.GiaThanh);
+    }
+
+    public string GetAllCDsInfo()
+    {
+        string info = "";
+        foreach (var cd in cds)
+        {
+            if (cd != null)
+                info += cd.ToString() + "\n";
+        }
+        return info;
+    }
+
+    public void SortByPriceDescending()
+    {
+        cds = cds.Where(x => x != null).OrderByDescending(x => x.GiaThanh).ToArray();
+    }
+
+    public void SortByTitleAscending()
+    {
+        cds = cds.Where(x => x != null).OrderBy(x => x.TuaCD).ToArray();
+    }
+}
+
+}
+
--- Buoi4/Buoi4/Program.cs
+++ Buoi4/Buoi4/Program.cs
@@ -10,7 +10,7 @@ namespace Buoi4
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Bai 7");
             Console.OutputEncoding = Encoding.UTF8;
             Console.InputEncoding = Encoding.Unicode;
             Point point = new Point("", 0.0f, 0.0f);
@@ -30,6 +30,46 @@ namespace Buoi4
         Console.WriteLine("Diện tích: {0}",hinhTron.TinhDienTich());
         Console.WriteLine("Chu vi: {0}",hinhTron.TinhChuVi());
         Console.ReadLine();
+
+        Console.WriteLine("Bai 8");  
+        Console.WriteLine("Nhập số lượng CD: ");
+        int n = int.Parse(Console.ReadLine());
+
+        CDList cdList = new CDList(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Nhập thông tin cho CD thứ {0}:",i + 1);
+            Console.Write("Mã CD: ");
+            int maCD = int.Parse(Console.ReadLine());
+            Console.Write("Tựa CD: ");
+            string tuaCD = Console.ReadLine();
+            Console.Write("Số bài hát: ");
+            int soBaiHat = int.Parse(Console.ReadLine());
+            Console.Write("Giá thành: ");
+            double giaThanh = double.Parse(Console.ReadLine());
+
+            CD cd = new CD(maCD, tuaCD, soBaiHat, giaThanh);
+
+            if (cdList.AddCD(cd))
+                Console.WriteLine("Thêm CD thành công!");
+            else
+                Console.WriteLine("Thêm CD không thành công! Mã CD trùng hoặc danh sách đã đầy.");
+        }
+
+        Console.WriteLine("\nDanh sách CD:");
+        Console.WriteLine(cdList.GetAllCDsInfo());
+
+        Console.WriteLine("Tổng giá thành của các CD: {0}",cdList.TotalCost());
+
+        Console.WriteLine("\nSắp xếp danh sách theo giá thành giảm dần:");
+        cdList.SortByPriceDescending();
+        Console.WriteLine(cdList.GetAllCDsInfo());
+
+        Console.WriteLine("\nSắp xếp danh sách theo tựa CD tăng dần:");
+        cdList.SortByTitleAscending();
+        Console.WriteLine(cdList.GetAllCDsInfo());
+    
         }
     }
 }
